f9ce4256f6c39150fa8b695b5d4fbf4c87c9c1a1
Shreya Patle <shreya.patle17@gmail.com>
2021-07-24 12:58:35 +0530

    added props

--- frontend/src/component/mainPage.js
+++ frontend/src/component/mainPage.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react'
+
+export default class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state =
+    {
+      tests: [
+        {
+          testName: "Calculus Test",
+          noOfQuestions: 20,
+          time: '30 min'
+        },
+        {
+          testName: "Average Test",
+          noOfQuestions: 20,
+          time: '30 min'
+        },
+        {
+          testName: "Chain Rule Test",
+          noOfQuestions: 20,
+          time: '30 min'
+        }
+
+      ]
+    }
+  }
 
 
-export default function mainPage() {
+  render(){
     return (
-        
-      <div class="card">
-  <div class="card-header">
-    Tests:
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Test Name:</h5>
-    <p class="card-text">No. of Questions:    Time:</p>
-    <a href="#" class="btn btn-primary">Start Now</a>
-  </div>
-</div>
+      <div className="container mt-2">
+        <h1>Tests:</h1>
+        {
+          this.state.tests.map((ele, index) => {
+            return <TestCard key={index} data={ele} />
+          })
+        }
+      </div>
     )
-}
+  }
+  }
+  
+  
+
+   export  function TestCard(props) {
+    console.log(props);
+  return (
+    <div>
+      <div className="card" style="width: 18rem;">
+        
+          <div className="card-body">
+            <h5 className="card-title">{props.data.testName}</h5>
+            <p className="card-text">No. of Questions:{props.data.noOfQuestions} Time:{props.data.time}</p>
+            <a href="#" className="btn btn-primary">Start Now</a>
+          </div>
+        
+      </div>
+      </div>
+      )
+  }
+
